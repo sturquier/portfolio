b96b26e5c1028d754a57744f42b788023b0e022d
sturquier <turquiersimon@hotmail.fr>
2023-11-09 16:14:17 +0100

    Add homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <main>
-        <div>HOME</div>
+    <main className="flex flex-col items-center justify-center h-screen text-center px-4">
+      <h1 className="text-4xl font-bold mb-2">{`Hey, I'm Simon Turquier 👋`}</h1>
+      <p className="text-xl">Lead Developer based in Paris, France</p>
     </main>
-  )
+  );
 }
