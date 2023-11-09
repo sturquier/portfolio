e4677a40bfe79eea65298c1a97fdb880af6d15d5
sturquier <turquiersimon@hotmail.fr>
2023-11-09 15:22:27 +0100

    Lint files & fix footer sticky position

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -8,42 +8,42 @@ import { usePathname } from 'next/navigation'
 import menuIcon from '../../public/assets/icons/menu.svg'
 
 export default function Navbar() {
-    const pathname = usePathname();
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const pathname = usePathname()
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
 
     useEffect(() => {
         const handleOutsideClick = (event: TouchEvent) => {
-            const target = event.target as Element;
+            const target = event.target as Element
      
             if (isMenuOpen && !target.closest('#navbar-menu')) {
-                setIsMenuOpen(false);
+                setIsMenuOpen(false)
             }
-        };
+        }
 
-        document.addEventListener('click', handleOutsideClick as EventListener);
+        document.addEventListener('click', handleOutsideClick as EventListener)
 
         return () => {
-            document.removeEventListener('click', handleOutsideClick as EventListener);
-        };
-    }, [isMenuOpen]);
+            document.removeEventListener('click', handleOutsideClick as EventListener)
+        }
+    }, [isMenuOpen])
 
     const toggleMenu = (): void => {
-        setIsMenuOpen(!isMenuOpen);
-    };
+        setIsMenuOpen(!isMenuOpen)
+    }
 
-    const isActive = (path: string): boolean => path === pathname;
+    const isActive = (path: string): boolean => path === pathname
 
     const getLinkClassName = (path: string): string => {
-        const baseClasses = "block py-2 pl-3 pr-4 rounded md:p-0";
+        const baseClasses = "block py-2 pl-3 pr-4 rounded md:p-0"
     
         const activeClasses = isActive(path)
             ? "text-white bg-cyan-700 md:bg-transparent md:text-cyan-700"
-            : "text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-cyan-700";
+            : "text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-cyan-700"
 
-        const inactiveClasses = "dark:text-white md:dark:hover:text-cyan-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent";
+        const inactiveClasses = "dark:text-white md:dark:hover:text-cyan-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
 
-        return `${baseClasses} ${activeClasses} ${inactiveClasses}`;
-    };
+        return `${baseClasses} ${activeClasses} ${inactiveClasses}`
+    }
 
     return (
         <nav className="bg-white border-gray-200 dark:bg-gray-900">
@@ -56,7 +56,8 @@ export default function Navbar() {
                     aria-controls="navbar-menu"
                     aria-expanded={isMenuOpen}
                 >
-                    <Image src={menuIcon} alt="Open menu" width={20} height={20} />
+                    <span className="sr-only">Open menu</span>
+                    <Image src={menuIcon} alt="Open menu" width={24} height={24} />
                 </button>
                 <div className={`${isMenuOpen ? 'block' : 'hidden'} w-full md:block md:w-auto`} id="navbar-menu">
                     <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
