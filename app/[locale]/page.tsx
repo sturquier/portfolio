c479c1ab855bd3a86741b59bd40c9ce967a1b5fc
sturquier <turquiersimon@hotmail.fr>
2023-11-14 12:22:45 +0100

    Improve homepage subtitle

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -8,7 +8,11 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center h-screen text-center px-4">
       <h1 className="text-4xl font-bold mb-2">{translations('title')}</h1>
-      <p className="text-xl">{translations('subtitle')}</p>
+      <p className="text-xl">
+        {translations('subtitle')}
+        <span className="line-through">{translations('subtitle2')}</span>
+        {` ${translations('subtitle3')}`}
+      </p>
     </main>
   )
 }
